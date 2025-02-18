b717adfb01f172bfa20d0d996de98154f104c1f7
Eric Steepleton <ericws19@outlook.com>
2025-02-18 12:20:47 -0600

    add new items

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write a greeting
+const greeting = " You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// write a message with the combination
+let message = "this is the combo";
+// assign variables
+let num1 = 11 - 1;
+let num2 = 20 * 2;
+let num3 = 19 + 20;
+// write an alert
+alert (`Vault Code is ${num1}-${num2}-${num3}`);
